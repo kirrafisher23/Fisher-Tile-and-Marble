f7716456e7e68c4ec72cf6bbb2cbf57fed99c103
Kirra Fisher <103162191+kirrafisher23@users.noreply.github.com>
2024-10-29 13:26:37 -0400

    ABSOLUTE MESS
    will be fixed

--- index.js
+++ index.js
@@ -4,3 +4,16 @@ const navMenu = document.querySelector('.nav-menu');
 hamburger.addEventListener('click', () => {
   navMenu.classList.toggle('hide');
 });
+
+window.addEventListener('load', () => {
+  const image = document.querySelector('.image');
+  const content = document.querySelector('.content');
+
+  // Check if elements are found
+  if (image && content) {
+    image.style.opacity = '1';
+    content.style.opacity = '1';
+  } else {
+    console.error("Image or content element not found.");
+  }
+});
